7a207689e8efc42b0369993a49626af9213783d0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-10-19 11:22:03 -0300

    test: 💍 health route

--- src/routes/health/health.spec.ts
+++ src/routes/health/health.spec.ts
@@ -4,7 +4,7 @@ import { describe, expect, it } from 'vitest';
 import { app } from '../../app.js';
 
 describe('Health Check', () => {
-  it('should return 200 OK', async () => {
+  it('should return status code 200 OK', async () => {
     const response = await request(app.server).get('/health/check');
     expect(response.status).toBe(StatusCodes.OK);
   });
